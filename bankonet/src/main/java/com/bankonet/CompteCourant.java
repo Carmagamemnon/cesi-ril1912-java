c5ba56f54bb05c9377ccd6ed56e0107fdf7817cc
Kévin Peeters <kevin.peeters@viacesi.fr>
2020-05-07 10:00:31 +0200

    Ajout fonctions de test supplémentaires

--- bankonet/src/main/java/com/bankonet/CompteCourant.java
+++ bankonet/src/main/java/com/bankonet/CompteCourant.java
@@ -8,12 +8,16 @@ public class CompteCourant {
 	private double montantDecouvertAutorise;
 	private static int nbComptesCourants = 0;
 
+	public CompteCourant() {
+		CompteCourant.nbComptesCourants++;
+	}
+
 	public CompteCourant(String numero, String intitule, double solde, double montantDecouvertAutorise) {
+		this();
 		this.numero = numero;
 		this.intitule = intitule;
 		this.solde = solde;
 		this.numero = numero;
-		CompteCourant.nbComptesCourants++;
 		System.out.println("Compte créé : " + this.toString());
 	}
 
@@ -21,12 +25,33 @@ public class CompteCourant {
 		return this.solde;
 	}
 
+	public void setSolde(double montant) {
+		if (montant < this.montantDecouvertAutorise) {
+			throw new ArithmeticException("Le solde ne peut être inférieur au découvert max. autorisé.");
+		} else {
+			this.solde = montant;
+		}
+	}
+
 	public void crediter(double montant) {
-		this.solde += montant;
+		if (montant < 0) {
+			throw new ArithmeticException("Le montant doit être positif.");
+		} else {
+			this.solde += montant;
+		}
 	}
 
 	public void debiter(double montant) {
-		this.solde -= montant;
+		if (montant < 0) {
+			throw new ArithmeticException("Le montant doit être positif.");
+		} else {
+			double newSolde = this.solde - montant;
+			if (newSolde < this.montantDecouvertAutorise) {
+				throw new ArithmeticException("Ce compte ne peut être débité davantage.");
+			} else {
+				this.solde -= montant;
+			}
+		}
 	}
 
 	public static int getNbComptesCourants() {
